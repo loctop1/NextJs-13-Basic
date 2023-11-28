4c23aad6b642320ae0bb92011de5c7d4b969f52f
loctop1 <loc2003k22@gmail.com>
2023-11-28 07:58:42 +0700

    part 7: useSWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,21 +8,40 @@ import x from '@/styles/app.module.css'
 import y from '@/styles/loctop1.module.css'
 import AppTable from '@/components/app.table'
 import { useEffect } from 'react'
+import useSWR from 'swr'
+/**Hook này thường được sử dụng trong ứng dụng React để lấy và quản lý dữ liệu với sự tập trung vào việc cung cấp trải 
+ * nghiệm người dùng tốt thông qua các tính năng như caching và revalidation. */
 
 export default function Home() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  /**Đây là một hàm có tên là fetcher nhận một URL làm tham số.
+   * Nó sử dụng hàm fetch để tạo một yêu cầu mạng đến URL được chỉ định.
+   * Kết quả (res) được giả định là một phản hồi JSON, và res.json() được gọi để phân tích dữ liệu JSON.
+   * Toàn bộ quá trình trả về một Promise.*/
 
-  // Fetch API
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:8000/blogs')
-      const data = await res.json();
-      console.log('check response', data);
+  const { data, error, isLoading } = useSWR(
+    /**Dòng này sử dụng hook SWR (Stale-While-Revalidate) để lấy dữ liệu. */
+    'http://localhost:8000/blogs',
+    /**Tham số đầu tiên là URL ('http://localhost:8000/blogs') từ đó dữ liệu sẽ được lấy. */
+    fetcher,
+    /**Tham số thứ hai là hàm lấy dữ liệu (fetcher) sẽ được sử dụng để lấy dữ liệu. */
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
     }
-    fetchData();
-  }, [])
+    /**Tham số thứ ba là một đối tượng tùy chọn cho hook SWR, trong đó ba tùy chọn (revalidateIfStale, 
+     * revalidateOnFocus, và revalidateOnReconnect) được đặt là false. */
+  );
+  /**Kết quả của hook useSWR được giải tỏa thành ba biến: data (dữ liệu đã lấy), error (bất kỳ lỗi nào xảy ra trong quá 
+   * trình lấy dữ liệu), và isLoading (một giá trị boolean chỉ ra liệu dữ liệu đang được lấy hay không). */
+  console.log('check data: ', data)
 
   return (
     <div>
+      <div>{data?.length}</div>
+      {/* Dấu ?. là toán tử kiểm tra động (optional chaining), đảm bảo rằng nếu data là null hoặc undefined, thì không 
+      có lỗi xảy ra và biểu thức sẽ trả về undefined. */}
       <ul>
         <li className={x['red']}>
           {/* Giả sử x là một đối tượng chứa các lớp CSS, và 'red' có vẻ như là một trong các lớp CSS trong đối tượng x.
